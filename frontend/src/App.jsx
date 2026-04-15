590c4ef60c2194d147debaf8d74f40449b2fd887
EmaSantoro <emsantoro@frba.utn.edu.ar>
2026-04-15 17:22:03 -0300

    whatsapp button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import CustomCursor from './components/CustomCursor';
 import ScrollProgress from './components/ScrollProgress';
+import WhatsAppButton from './components/WhatsAppButton'; // <-- 1. Importamos el botón
 import Home from './pages/Home';
 import FAQ from './pages/FAQ';
 import ServicioDetalle from './pages/ServicioDetalle';
@@ -11,41 +12,42 @@ import TrabajoDetalle from './pages/TrabajoDetalle';
 import NotFound from './pages/NotFound';
 
 const PageWrapper = ({ children }) => (
-  <motion.div
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    exit={{ opacity: 0 }}
-    transition={{ duration: 0.25 }}
-  >
-    {children}
-  </motion.div>
+    <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.25 }}
+    >
+        {children}
+    </motion.div>
 );
 
 function AppContent() {
-  const location = useLocation();
-  return (
-    <>
-      <CustomCursor />
-      <ScrollProgress />
-      <Navbar />
-      <AnimatePresence mode="wait">
-        <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
-          <Route path="/faq" element={<PageWrapper><FAQ /></PageWrapper>} />
-          <Route path="/servicios/:slug" element={<PageWrapper><ServicioDetalle /></PageWrapper>} />
-          <Route path="/trabajos/:id" element={<PageWrapper><TrabajoDetalle /></PageWrapper>} />
-          <Route path="*" element={<PageWrapper><NotFound /></PageWrapper>} />
-        </Routes>
-      </AnimatePresence>
-      <Footer />
-    </>
-  );
+    const location = useLocation();
+    return (
+        <>
+            <CustomCursor />
+            <ScrollProgress />
+            <WhatsAppButton /> {/* <-- 2. Inyectamos el botón globalmente */}
+            <Navbar />
+            <AnimatePresence mode="wait">
+                <Routes location={location} key={location.pathname}>
+                    <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
+                    <Route path="/faq" element={<PageWrapper><FAQ /></PageWrapper>} />
+                    <Route path="/servicios/:slug" element={<PageWrapper><ServicioDetalle /></PageWrapper>} />
+                    <Route path="/trabajos/:id" element={<PageWrapper><TrabajoDetalle /></PageWrapper>} />
+                    <Route path="*" element={<PageWrapper><NotFound /></PageWrapper>} />
+                </Routes>
+            </AnimatePresence>
+            <Footer />
+        </>
+    );
 }
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <AppContent />
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <AppContent />
+        </BrowserRouter>
+    );
 }
